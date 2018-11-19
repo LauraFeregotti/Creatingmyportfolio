9a7ae77a52cecaabab349073b6bd524949fabfb0
unknown <laura.feregotti@gmail.com>
2018-11-19 14:42:32 +0100

    First steps

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,19 @@
 <template>
   <div id="app">
     <div class="row">
-      <div class="col-lg-offset-5 col-lg-2 col-lg-offset-5">
-        <img src="" alt="">
+      <div class="col-lg-offset-1 col-lg-1 font navtext">
+        <span>about</span>
+      
+      </div>
+      <div class=" col-lg-1 font navtext">
+        <span>work</span>
+        
+      </div>
+      <div class="col-lg-2 col-lg-offset-1 navlogo">
+        <img class="imagenlogo " src="images/titulo.png" alt="titulo">
+      </div>
+      <div class=" col-lg-1 col-lg-offset-3 font navtext">
+        <span>contact</span>
       </div>
     </div>
   </div>
@@ -20,5 +31,28 @@ export default {
 </script>
 
 <style>
+@font-face {
+    font-family: 'abeer';
+    src: url("/fonts/AbeerDemo.ttf");
+}
+.imagenlogo{
+  width: 338pt;
+  height: 277.541pt;
 
+}
+.font{
+font-family: 'abeer';
+font-size: 17pt;
+}
+
+.navlogo{
+  margin-top: 31px;
+}
+.navtext{
+margin-top: 111px;
+}
+body{
+  border: 1rem solid rgb(216, 218, 240, .6);
+
+}
 </style>

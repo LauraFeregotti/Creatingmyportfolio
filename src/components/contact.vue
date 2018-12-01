e1879aeffaff323e3d1a32205feecde9fc5d2749
unknown <laura.feregotti@gmail.com>
2018-12-02 00:01:18 +0100

    responsive lg screen changed

--- src/components/contact.vue
+++ src/components/contact.vue
@@ -12,14 +12,14 @@
         <div class="col-lg-3 col-md-3 col-sm-3 col-xs-3   sentence-contact">
           <p>laura.feregotti@gmail.com</p>
         </div>
-        <div class="col-lg-1 col-md-offset-4 col-md-1  col-sm-offset-4 col-sm-1 col-xs-offset-2 col-xs-3">
+        <div class="col-lg-offset-1 col-lg-1 col-md-offset-4 col-md-1  col-sm-offset-4 col-sm-1 col-xs-offset-2 col-xs-3">
           <img
             src="@/assets/images/icons/phone.png"
             alt="phone"
             class="icon center-block"
           >
         </div>
-        <div class="col-lg-3 col-md-3 col-sm-3 col-xs-2  sentence-contact">
+        <div class="col-lg-2 col-md-3 col-sm-3 col-xs-2  sentence-contact">
           <p>+4917674563112</p>
         </div>
       </div>
@@ -35,7 +35,7 @@
          <a href="https://www.linkedin.com/in/laura-feregotti/"  class="link"> <p>https://www.linkedin
             .com/in/laura-feregotti/</p></a>
         </div>
-        <div class="col-lg-1 col-md-offset-4 col-md-1 col-sm-offset-4 col-sm-1 col-xs-offset-3 col-xs-3">
+        <div class=" col-lg-offset-1 col-lg-1 col-md-offset-4 col-md-1 col-sm-offset-4 col-sm-1 col-xs-offset-3 col-xs-3">
           <img
             src="@/assets/images/icons/location.png"
             alt="location"
@@ -64,7 +64,7 @@
   margin-top: 64px;
   width: 27px;
   height: 27px;
-  text-align: right;
+
 }
 a:active,
 a:focus,
